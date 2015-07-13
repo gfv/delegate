bc9ec109f7fad7ed166b20323b5daf7c018d077b
mihver1 <self@mihver1.ru>
2015-07-13 22:27:59 +0300

    username fix

--- scripts.py
+++ scripts.py
@@ -13,7 +13,7 @@ def launch_script(request, policy):
         arguments = []
 
     if b"PROVIDE_USERNAME" in policy.parameters:
-        os.environ["DELEGATE_USERNAME"] = policy.user
+        os.environ["DELEGATE_USERNAME"] = request.signed_with.decode()
 
     process = subprocess.Popen(
         args=[scripts[request.script.decode()]["cmd_line"]] + scripts[request.script.decode()]["default_arguments"] + arguments,
